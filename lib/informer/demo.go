f0259bad867c0333b2adf629c5e6176adfaba5de
colstuwjx <colstuwjx@gmail.com>
2019-05-26 14:30:02 +0800

    Add ingress/endpoint resources.

--- lib/informer/demo.go
+++ lib/informer/demo.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"reflect"
 	"syscall"
 	"time"
 
 	corev1 "k8s.io/api/core/v1"
+	extensions "k8s.io/api/extensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/client-go/informers"
@@ -19,14 +21,32 @@ import (
 )
 
 type Informer struct {
-	Pod cache.SharedIndexInformer
+	Pod      cache.SharedIndexInformer
+	Ingress  cache.SharedIndexInformer
+	Endpoint cache.SharedIndexInformer
 }
 
 func (i *Informer) Run(stopCh chan struct{}) {
 	go i.Pod.Run(stopCh)
+	go i.Endpoint.Run(stopCh)
 
 	if !cache.WaitForCacheSync(stopCh,
 		i.Pod.HasSynced,
+	) {
+		err := fmt.Errorf("Timed out waiting for caches to sync")
+		runtime.HandleError(err)
+	}
+
+	// in big clusters, deltas can keep arriving even after HasSynced
+	// functions have returned 'true'
+	time.Sleep(1 * time.Second)
+
+	// we can start syncing ingress objects only after other caches are
+	// ready, because ingress rules require content from other listers, and
+	// 'add' events get triggered in the handlers during caches population.
+	go i.Ingress.Run(stopCh)
+	if !cache.WaitForCacheSync(stopCh,
+		i.Ingress.HasSynced,
 	) {
 		err := fmt.Errorf("Timed out waiting for caches to sync")
 		runtime.HandleError(err)
@@ -37,8 +57,18 @@ type PodLister struct {
 	cache.Store
 }
 
+type IngressLister struct {
+	cache.Store
+}
+
+type EndpointLister struct {
+	cache.Store
+}
+
 type Lister struct {
-	Pod PodLister
+	Pod      PodLister
+	Ingress  IngressLister
+	Endpoint EndpointLister
 }
 
 type K8sStore struct {
@@ -62,6 +92,12 @@ func NewK8sStore(
 	store.informers.Pod = infFactory.Core().V1().Pods().Informer()
 	store.listers.Pod.Store = store.informers.Pod.GetStore()
 
+	store.informers.Ingress = infFactory.Extensions().V1beta1().Ingresses().Informer()
+	store.listers.Ingress.Store = store.informers.Ingress.GetStore()
+
+	store.informers.Endpoint = infFactory.Core().V1().Endpoints().Informer()
+	store.listers.Endpoint.Store = store.informers.Endpoint.GetStore()
+
 	podEventHandler := cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
 			// "k8s.io/apimachinery/pkg/apis/meta/v1" provides an Object
@@ -84,6 +120,58 @@ func NewK8sStore(
 		},
 	}
 	store.informers.Pod.AddEventHandler(podEventHandler)
+
+	ingEventHandler := cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			ing := obj.(*extensions.Ingress)
+			klog.Infof("Added ingress: %v", ing)
+		},
+
+		DeleteFunc: func(obj interface{}) {
+			ing, ok := obj.(*extensions.Ingress)
+			if !ok {
+				// If we reached here it means the ingress was deleted but its final state is unrecorded.
+				tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
+				if !ok {
+					klog.Errorf("couldn't get object from tombstone %#v", obj)
+					return
+				}
+				ing, ok = tombstone.Obj.(*extensions.Ingress)
+				if !ok {
+					klog.Errorf("Tombstone contained object that is not an Ingress: %#v", obj)
+					return
+				}
+			}
+
+			klog.Infof("Deleted ingress: %v", ing)
+		},
+
+		UpdateFunc: func(old, cur interface{}) {
+			oldIng := old.(*extensions.Ingress)
+			curIng := cur.(*extensions.Ingress)
+			klog.Infof("Updated ingress: %v, old: %v", curIng, oldIng)
+		},
+	}
+	store.informers.Ingress.AddEventHandler(ingEventHandler)
+
+	epEventHandler := cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			ep := obj.(*corev1.Endpoints)
+			klog.Infof("Added endpoint: %v", ep)
+		},
+		DeleteFunc: func(obj interface{}) {
+			klog.Infof("Deleted endpoint: %v", obj)
+		},
+		UpdateFunc: func(old, cur interface{}) {
+			oep := old.(*corev1.Endpoints)
+			cep := cur.(*corev1.Endpoints)
+			if !reflect.DeepEqual(cep.Subsets, oep.Subsets) {
+				klog.Infof("Updated endpoint: %v, old: %v", cep, oep)
+			}
+		},
+	}
+	store.informers.Endpoint.AddEventHandler(epEventHandler)
+
 	return store
 }
 
